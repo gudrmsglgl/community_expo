c61fbce09803216e31feb9e7412c21f1e06f7b9b
gudrmsglgl <gudrmsglgl@gmail.com>
2026-03-09 16:15:53 +0900

    feat: 로그인 화면 구성

--- app/auth/index.tsx
+++ app/auth/index.tsx
@@ -1,10 +1,50 @@
 import Logo from "@/assets/images/logo.svg";
+import CTAButton from "@/components/CTAButton";
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function AuthScreen() {
   return (
-    <SafeAreaView edges={["left", "right", "bottom"]}>
-      <Logo />
+    <SafeAreaView edges={["left", "right", "bottom"]} style={styles.container}>
+      <LogoContainer />
+      <View style={styles.buttons}>
+        <CTAButton title="로그인하기" variant="Filled" size="Large" />
+        <Link href="/auth/signup">
+          <Text style={styles.link}>이메일로 가입하기</Text>
+        </Link>
+      </View>
     </SafeAreaView>
   );
 }
+
+function LogoContainer() {
+  return (
+    <View style={styles.logoContainer}>
+      <Logo />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    justifyContent: "space-between",
+  },
+  logoContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttons: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 32,
+    gap: 20,
+  },
+  link: {
+    textDecorationLine: "underline",
+    fontSize: 14,
+  },
+});
---
+++ app/auth/signup.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function SignupScreen() {
+  return (
+    <View>
+      <Text>이메일로 가입하기</Text>
+    </View>
+  );
+}
