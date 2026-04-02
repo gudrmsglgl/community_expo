3c3350dbbd3818dc5cdcfa9e55120d4551a64fa9
gudrmsglgl <gudrmsglgl@gmail.com>
2026-04-02 15:14:53 +0900

    feat: GET-POST API 및 hook 추가

---
+++ hooks/useGetInfinitePosts.ts
@@ -0,0 +1,15 @@
+import { getPosts } from "@/api/post";
+import { queryKey } from "@/constants/queryKey";
+import { useInfiniteQuery } from "@tanstack/react-query";
+
+export default function useGetInfinitePosts() {
+  return useInfiniteQuery({
+    queryFn: ({ pageParam }) => getPosts(pageParam),
+    queryKey: [queryKey.POST, queryKey.GET_POSTS],
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, allPages) => {
+      const lastPost = lastPage[lastPage.length - 1];
+      return lastPost ? allPages.length + 1 : undefined;
+    },
+  });
+}
