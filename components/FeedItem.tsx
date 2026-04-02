44650c5bf79251d3f6f68ef02fed78a87117408e
gudrmsglgl <gudrmsglgl@gmail.com>
2026-04-02 15:16:06 +0900

    refactor: FeedItem 프로필 컴포넌트 추가 및 데이터 매핑

--- components/FeedItem.tsx
+++ components/FeedItem.tsx
@@ -1,16 +1,30 @@
+import useAuth from "@/hooks/queries/useAuth";
 import { Ionicons, MaterialCommunityIcons, Octicons } from "@expo/vector-icons";
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import { colors } from ".";
 import { Post } from "../types";
+import Profile from "./Profile";
 
 interface FeedItemProps {
   post: Post;
 }
 
 export default function FeedItem({ post }: FeedItemProps) {
-  const isLiked = true;
+  const { auth } = useAuth();
+  const isLiked = post.likes.some(
+    (like) => Number(like.userId) === Number(auth.id),
+  );
+
   return (
     <View style={styles.container}>
+      <View style={styles.profileContainer}>
+        <Profile
+          onPress={() => {}}
+          imageUri={post.author.imageUri}
+          nickname={post.author.nickname}
+          createdAt={post.createdAt}
+        />
+      </View>
       <View style={styles.contentContainer}>
         <Text style={styles.title}>{post.title}</Text>
         <Text style={styles.description} numberOfLines={3} ellipsizeMode="tail">
@@ -18,21 +32,21 @@ export default function FeedItem({ post }: FeedItemProps) {
         </Text>
       </View>
       <View style={styles.iconContainer}>
-        <IconItem count={10} isActive={isLiked}>
+        <IconItem count={post.likes.length} isActive={isLiked}>
           <Octicons
             name={isLiked ? "heart-fill" : "heart"}
             size={16}
             color={isLiked ? colors.ORANGE_600 : colors.BLACK}
           />
         </IconItem>
-        <IconItem count={10}>
+        <IconItem count={post.commentCount}>
           <MaterialCommunityIcons
             name="comment-processing-outline"
             size={16}
             color={colors.BLACK}
           />
         </IconItem>
-        <IconItem count={10}>
+        <IconItem count={post.viewCount}>
           <Ionicons name="eye-outline" size={16} color="black" />
         </IconItem>
       </View>
@@ -63,8 +77,13 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: colors.WHITE,
   },
+  profileContainer: {
+    paddingHorizontal: 16,
+    paddingTop: 16,
+  },
   contentContainer: {
     paddingHorizontal: 16,
+    paddingTop: 28,
   },
   title: {
     fontSize: 18,
