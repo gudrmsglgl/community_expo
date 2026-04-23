bdd2d08aedc4def7ab18b250166025faf30bb0ab
gudrmsglgl <gudrmsglgl@gmail.com>
2026-04-23 17:31:33 +0900

    feat: 이미지 미리보기 리스트 컴포넌트 추가

---
+++ components/ImagePreviewList.tsx
@@ -0,0 +1,40 @@
+import { BASE_URL } from "@/api/axios";
+import { ImageUri } from "@/types";
+import { Image, Pressable, StyleSheet } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+
+export default function ImagePreviewList({
+  imageUris = [],
+}: {
+  imageUris: ImageUri[];
+}) {
+  return (
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={styles.container}
+    >
+      {imageUris.map(({ uri }, index) => (
+        <Pressable key={uri + index} style={styles.imageContainer}>
+          <Image source={{ uri: `${BASE_URL}/${uri}` }} style={styles.image} />
+        </Pressable>
+      ))}
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    gap: 5,
+    flexGrow: 1,
+  },
+  imageContainer: {
+    width: 90,
+    height: 90,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 8,
+  },
+});
